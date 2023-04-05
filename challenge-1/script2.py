38abde3e72bd010a569cbccff5ed4fcfb5d0e949
Paolo <paololongo.git@gmail.com>
2023-04-05 20:28:49 +0200

    first commit

--- challenge-1/script2.py
+++ challenge-1/script2.py
@@ -1,33 +1,52 @@
-from scapy.all import *
-
-load_contrib("coap")
-
-pkts = rdpcap('./file.pcapng')
-
-ips = ['134.102.218.18']
+from scapy.contrib.coap import CoAP
+from scapy.layers.dns import DNS
+from scapy.layers.inet import IP
+from scapy.utils import rdpcap
 
 count = 0
 helloCount = 0
+ips = set([])
+packets = rdpcap('./file.pcapng')
 
-for pkt1 in pkts:
+# For each packet in the pcap file
+for i in range(0, len(packets)):
+    # Get the packet
+    packet = packets[i]
+    # If the packet has DNS and the QR flag is set to 1, meaning it is a response
+    if DNS in packet and packet[DNS].qr == 1:
+        # Loop through the answer section of the DNS response
+        for i in range(packet[DNS].ancount):
+            # If the answer is an IP record for the HiveMQ broker
+            if packet[DNS].an[i].rrname == b'coap.me.' and packet[DNS].an[i].type in [1, 28]:
+                # Add the IP address of the HiveMQ broker to the set
+                ips.add(packet[DNS].an[i].rdata)
+
+for i in range(0, len(packets)):
+    # Get the packet
+    delete = packets[i]
     # and CoAP in pkt and pkt[CoAP].code == "4.04":
-    if CoAP in pkt1 and pkt1[CoAP].code == 4 and pkt1[IP].dst == '134.102.218.18':
-        for option in pkt[CoAP].options:
-            print(option)
+    if CoAP in delete and delete[CoAP].code == 4 and delete[IP].dst == '134.102.218.18':
+        isHelloPath = False
+        for option in delete[CoAP].options:
             if option[0] == 'Uri-Path' and option[1] == b'hello':
-                helloCount += 1
-        for pkt in pkts:
-            if (IP in pkt and CoAP in pkt and (pkt[CoAP].code >= 65 or pkt[CoAP].code <= 95) and
-                    ((pkt1[CoAP].token != b'' and pkt1[CoAP].token == pkt[CoAP].token) or pkt1[CoAP].msg_id == pkt[CoAP].msg_id) and
-                    pkt1[IP].dst == pkt[IP].src and pkt1[IP].src == pkt[IP].dst):
-                count += 1
-
-                for option in pkt[CoAP].options:
-                    print(option)
-                    if option[0] == 'Uri-Path' and option[1] == b'hello':
-                        helloCount += 1
-
+                isHelloPath = True
                 break
+
+        didFound = False
+        for response in packets:
+            if (IP in response and CoAP in response and
+                    (response[CoAP].code >= 65 or response[CoAP].code <= 95) and
+                    ((response[CoAP].token != b'' and response[CoAP].token == delete[CoAP].token) or response[
+                        CoAP].msg_id == delete[CoAP].msg_id) and
+                    response[IP].dst == delete[IP].src and response[IP].src == delete[IP].dst):
+                didFound = True
+                break
+
+        if not didFound:
+            if isHelloPath:
+                helloCount += 1
+            else:
+                count += 1
 
 print("count: ", count)
 print("tot count: ", helloCount)
--- challenge-1/script5.py
+++ challenge-1/script5.py
@@ -1,22 +1,24 @@
-from scapy.utils import rdpcap
-from scapy.layers.inet import IP
 from scapy.contrib.mqtt import MQTT
-from scapy.layers.dns import DNS, DNSRR
+from scapy.layers.dns import DNS
+from scapy.layers.inet import IP
+from scapy.utils import rdpcap
 
-ips = []
 count = 0
+ips = set([])
 packets = rdpcap('./file.pcapng')
-ips = ["52.29.173.150", "3.65.137.17"]
 
+# For each packet in the pcap file
 for i in range(0, len(packets)):
+    # Get the packet
     packet = packets[i]
+    # If the packet has DNS and the QR flag is set to 1, meaning it is a response
     if DNS in packet and packet[DNS].qr == 1:
         # Loop through the answer section of the DNS response
-       print("---------")
-       for i in range(packet[DNS].ancount):
-           print(packet[DNSRR][i].rdata)
-
-print(ips)
+        for i in range(packet[DNS].ancount):
+            # If the answer is an IP record for the HiveMQ broker
+            if packet[DNS].an[i].rrname == b'broker.hivemq.com.' and packet[DNS].an[i].type in [1, 28]:
+                # Add the IP address of the HiveMQ broker to the set
+                ips.add(packet[DNS].an[i].rdata)
 
 # For each packet in the pcap file
 for i in range(0, len(packets)):
@@ -27,7 +29,7 @@ for i in range(0, len(packets)):
             # If the packet is directed to the HiveMQ broker
             IP in conn and conn[IP].dst in ips):
         # For each packet after the current packet
-        for j in range(i+1, len(packets)):
+        for j in range(i + 1, len(packets)):
             # Get the packet
             pubOrConn = packets[j]
             # If the packet has MQTT and the type is 3 (PUBLISH)
